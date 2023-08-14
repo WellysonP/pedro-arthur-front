00a2eaad846b5baed68fc22d0bb42b8d4fb94b28
francisco-cruz <franaraujosilva2017.com>
2023-08-14 17:14:04 -0300

    [REFACT]: moldura cresce 100% da altura

--- src/components/layout/content-body.js
+++ src/components/layout/content-body.js
@@ -5,10 +5,10 @@ import PedroArthur  from "../../assets/images/pedro.png";
 
 const ContentBody = ({ children }) => {
   return (
-    <Flex bg="white" direction="column" alignItems="center" pos="relative" maxW="650px" minH="100vh" m="0 auto" padding="32px 24px">
+    <Flex bg="white" direction="column" alignItems="center" pos="relative" maxW="650px" minH="100vh" h="100%"  m="0 auto" padding="32px 24px">
       <Image width="100%" pos="absolute" top="0" src={BgUp} />
       <Image width="100%" pos="absolute" bottom="0"  src={BgDown} />
-      <Flex direction="column" width="100%" padding="96px 16px 40px 16px" alignItems="center" justify="center" border="12px solid #7E7935" borderRadius="999px 999px 0px 0px" h="100%" >
+      <Flex direction="column" width="100%" h="100%" padding="96px 16px 40px 16px" alignItems="center" justify="flex-start" border="12px solid #7E7935" borderRadius="999px 999px 0px 0px" h="100%" >
       <Image width="280px" src={PedroArthur} />
         {children}
       </Flex>
