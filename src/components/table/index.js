989e611ac54d4a12e39a3406d03b04ff2af118f6
WellysonP <100440445+WellysonP@users.noreply.github.com>
2023-08-16 19:08:23 -0300

    [FEATURE]: adicionado axios ao projeto, iniciado consumo de api

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -9,11 +9,11 @@ const TableGuests = ({ guests, handleEditClick }) => {
           <Tbody>
             {guests.map((guest, index) => (
               <Tr key={index} bg={index % 2 === 0 ? "#fff" : "#D9E1A5"} borderBottom={index % 2 === 0 ? "#fff" : "#7E7935"} borderTop={index % 2 === 0 ? "#fff" : "#7E7935"} h="60px">
-                <Td fontWeight="bold">{guest.nome}</Td>
-                <Td fontWeight="bold">{guest.qtd}</Td>
-                <Td fontWeight="bold" color={guest.status ==="pendente" ? "#D88000": "#584F4A"}>{guest.status}</Td>
+                <Td fontWeight="bold">{guest.name}</Td>
+                <Td fontWeight="bold">{guest.quantity}</Td>
+                <Td fontWeight="bold" color={guest.isConfirmed === 0 ? "#D88000" : "#584F4A"}>{guest.isConfirmed === 0 ? "Pendente" : "Confirmado"}</Td>
                 <Td>
-                  <button style={{background: "#9A5B0D", borderRadius: "8px"}} onClick={() => handleEditClick(guest)}>Editar</button>
+                  <button style={{ background: "#9A5B0D", borderRadius: "8px" }} onClick={() => handleEditClick(guest)}>Editar</button>
                 </Td>
               </Tr>
             ))}
